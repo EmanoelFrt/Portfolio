ef04dc00299fab98154aa69a98e13c401e245037
Emanoel Fortuna <emanoellfortuna@gmail.com>
2023-09-17 14:07:13 -0300

    Atualização para deixar o SobreMim responsivo

--- src/components/SobreMim.js
+++ src/components/SobreMim.js
@@ -30,6 +30,15 @@ const SobreMim = () => {
         <div className="logo-sobre-mim">
           <img src={Logo_git}/>
         </div>
+      </div>          
+      <div className="text-container">
+        <h1>Sobre mim</h1>
+        <p>
+          Sou desenvolvedor Delphi, .Net e AWS há 4 anos. Como freelancer,
+          participei de projetos em .Net, Python e React. Sempre foco no
+          desempenho de minhas aplicações e na interface amigável para o
+          usuário.
+        </p>
       </div>
       <div className="logo-container-right">
         <div className="logo-sobre-mim">
@@ -47,16 +56,7 @@ const SobreMim = () => {
         <div className="logo-sobre-mim">
           <img src={Logo_nodejs}/>
         </div>
-      </div>            
-      <div className="text-container">
-        <h1>Sobre mim</h1>
-        <p>
-          Sou desenvolvedor Delphi, .Net e AWS há 4 anos. Como freelancer,
-          participei de projetos em .Net, Python e React. Sempre foco no
-          desempenho de minhas aplicações e na interface amigável para o
-          usuário.
-        </p>
-      </div>
+      </div>  
     </div>
   );
 };
