20360c77cd800b6c6d4c57bece3b147d12795ac0
Emanoel Fortuna <emanoellfortuna@gmail.com>
2023-10-01 18:51:41 -0300

    Ajustes em Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import './Header.css'
-import FotoBanner from '../../assets/img/FotoBanner.jpg';
+import LogoSite from '../../assets/img/LogoSite.png';
 
 const Header = ({acao}) => {
   return (
     <header className={ acao ? 'ativaCor' : ''}>
       <div className="logo-header">
-        <img src={FotoBanner}/>
+        <img src={LogoSite}/>
       </div>
 
       <div className='menu'>
         <ul>
+          <li><a href="#">Início</a></li>
           <li><a href="#">Sobre</a></li>
+          <li><a href="#">Projetos</a></li>
           <li><a href="#">Contato</a></li>
         </ul>
       </div>
